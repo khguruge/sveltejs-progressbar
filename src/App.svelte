0abc5e8a8ebceabaa0e1e4c90b8df20bbd4adb2b
khguruge <gkasunharshana@gmail.com>
2022-06-09 12:28:46 +0530

    init

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Progress from './lib/Progress.svelte'
+</script>
+
+<main>
+  <Progress />
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
