079029b1b2a3b0aae868834e66169a9d2ba6c776
khguruge <gkasunharshana@gmail.com>
2022-06-09 12:31:36 +0530

    init

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,36 @@
-<script>
+<!-- <script>
   import Progress from './lib/Progress.svelte'
 </script>
 
 <main>
   <Progress progress={40} color={'pink'}/>
+</main> -->
+<script>
+  export let progress = 50;
+  export let color = "blue";
+
+  if (progress > 100) {
+    progress = 100;
+  } else if (progress <= 0) {
+    progress = 0;
+  }
+</script>
+
+<main>
+  <div class="outer" style=" border: 1px solid {color};">
+    <div class="inner" style="width : {progress}%; background : {color}" />
+  </div>
 </main>
+
+<style>
+  .inner,
+  .outer {
+    height: 40px;
+    border-radius: 30px;
+  }
+
+  .outer {
+    width: 100%;
+    background: transparent;
+  }
+</style>
