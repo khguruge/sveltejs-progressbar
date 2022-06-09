e7b96ba2dadafcbd8431b12446a5abe862a230fb
khguruge <gkasunharshana@gmail.com>
2022-06-09 12:30:43 +0530

    init

--- src/App.svelte
+++ src/App.svelte
@@ -5,16 +5,3 @@
 <main>
   <Progress progress={40} color={'pink'}/>
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-</style>
